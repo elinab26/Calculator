05ab808ff7cb05679ee8553de39a3e5cd841c3ba
elinab26 <neveteli2005@gmail.com>
2025-02-27 13:15:55 +0200

    Added the input of the operator

--- calculator.s
+++ calculator.s
@@ -2,35 +2,57 @@
 .extern scanf
 .section .rodata
 num1: .string "Please enter the first number: "
-num2: .string "\nPlease enter the second number: "
-op: .string "\nPlease enter the arithmetic operation: "
+num2: .string "Please enter the second number: "
+op: .string "Please enter the arithmetic operation: "
 num_fmt: .string "%d"
-char_fmt: .string "%c"
+char_fmt: .string " %c"
+
 
 .section .text
 .globl main
 .type	main, @function 
 main:
     # Enter
-    pushq   %rbp
-    movq    %rsp, %rbp  
+    pushq %rbp
+    movq %rsp, %rbp  
 
-    #print the first number string
-    leaq    num1(%rip), %rdi
-    xorq    %rax, %rax
-    call    printf
+    #print the num1 string
+    leaq num1(%rip), %rdi
+    xorq %rax, %rax
+    call printf
 
-    #get the first number from the user
-    leaq    num_fmt(%rip), %rdi
-    movq    %rsp, %rsi
-    xorq    %rax, %rax
-    call    scanf
+    #get the first number and save it in the stack
+    leaq num_fmt(%rip), %rdi
+    leaq (%rsp), %rsi
+    xorq %rax, %rax
+    call scanf
 
-    #pushing the first number to the stack
-    pushq   %rsi
+    #print the num2 string
+    leaq num2(%rip), %rdi
+    xorq %rax, %rax
+    call printf
+
+    #get the second number and save it in the stack
+    leaq num_fmt(%rip), %rdi
+    leaq 8(%rsp), %rsi
+    xorq %rax, %rax
+    call scanf
+
+    #print the op string
+    leaq op(%rip), %rdi
+    xorq %rax, %rax
+    call printf 
+
+    #get the operator and save it in the stack
+    leaq char_fmt(%rip), %rdi
+    leaq 16(%rsp), %rsi
+    xorq %rax, %rax
+    call scanf
 
 
 exit:
-    movq    %rbp, %rsp
-    popq    %rbp
-    ret
+    movq %rbp, %rsp
+    popq %rbp
+    movq $60, %rax      
+    movq $0, %rdi       # exit status
+    syscall            
